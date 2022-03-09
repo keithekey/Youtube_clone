54f1313451a87eb49a97ecc0cb5243f47342f43b
Jeongwoo Kim <80874359+keithekey@users.noreply.github.com>
2022-03-10 05:08:21 +0900

    20220309

--- src/server.js
+++ src/server.js
@@ -1,29 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-    return res.send("<h1>Welcome!</h1>");
-}
-
-const handleAbout = (req, res) => {
-    return res.send("<font color='0000FF'>My name is Jeongwoo. Nice to meet you!</font>");
-}
-
-const handleContact = (req, res) => {
-    return res.send("If you have any question, please contact to <a href=''>keithey@outlook.com</a>");
-}
-
-const handleLogin = (req, res) => {
-    return res.send("<b>This is login page.</b>");
-}
-
-app.get("/",  handleHome);
-app.get("/about", handleAbout);
-app.get("/contact", handleContact);
-app.get("/login", handleLogin);
+  app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+  app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
