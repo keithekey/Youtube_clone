5f9016b9980b9c5c25289a0f37cb0f95c4810fed
Jeongwoo Kim <80874359+keithekey@users.noreply.github.com>
2022-03-14 23:39:44 +0900

    20220314

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,36 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
+import express from "express";
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        }
+    ];
+    return res.render("home", { pageTitle: "Home", videos });
 }
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit Videos");
-}
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => {
