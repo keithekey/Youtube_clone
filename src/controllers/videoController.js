e2265d07f323811438758e11f4ba31d5dc565c7d
Jeongwoo Kim <80874359+keithekey@users.noreply.github.com>
2022-03-18 22:43:15 +0900

    20220318

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,39 +1,43 @@
-import express from "express";
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1
-        }
-    ];
+export const home = async (req, res) =>  {
+    const videos = await Video.find({});
     return res.render("home", { pageTitle: "Home", videos });
 }
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search Videos");
-export const upload = (req, res) => res.send("Upload Video");
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    return res.send("Delete Video");
+export const watch = async (req, res) => {
+    const id = req.params.id;
+    const video = await Video.findById(id);
+    if(video) {
+        return res.render("watch", { pageTitle: video.title, video });
+    }
+    return res.render("404", { pageTitle: "Video not found" });
+}
+export const getEdit = async (req, res) => {
+    const id = req.params.id;
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", { pageTitle: "Video not found" });
+    }
+    return res.render("edit", { pageTitle: "Editing Video", video });
+}
+export const postEdit = (req, res) => {
+    return res.redirect(`/videos/${id}`);
+}
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+}
+export const postUpload = async (req, res) => {
+    try{
+        const { title, description, hashtags } = req.body;
+        const video = new Video({
+            title,
+            description,
+            hashtags: hashtags.split(",").map((word) => `#${word}`)
+        });
+        await video.save();
+        return res.redirect("/");
+    } catch(error) {
+        console.log(error);
+        return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message });
+    }
 }
